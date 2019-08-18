467c94413e95a0d9be13dcd8e1779b93869d64a1
iomusashi <ionicsoft@gmail.com>
2019-08-18 14:02:17 +0800

    Created empty workspace using pod lib create tool

---
+++ Example/Pods/Target Support Files/StatefulCollectionView/StatefulCollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_StatefulCollectionView : NSObject
+@end
+@implementation PodsDummy_StatefulCollectionView
+@end
---
+++ Example/StatefulCollectionView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  StatefulCollectionView
+//
+//  Created by iomusashi on 08/18/2019.
+//  Copyright (c) 2019 iomusashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
